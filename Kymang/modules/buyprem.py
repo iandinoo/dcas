9c9b81f2fbf8ec07c51b62a4d2f04557d7a8bb60
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-29 21:56:19 +0700

    Update buyprem.py

--- Kymang/modules/buyprem.py
+++ Kymang/modules/buyprem.py
@@ -4,8 +4,10 @@ import asyncio
 import importlib
 
 from datetime import datetime, timedelta
-from pykeyboard import InlineKeyboard
 from pyrogram import Client, filters
 from pyrogram.types import *
 
+@bot.on_callback_query(filters.regex("month1"))
+async def month1(c: Bot, cb: CallbackQuery):
+    await cb.edit_message_text(C10, reply_markup=KL10)
 
