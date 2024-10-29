0bf6b5afa72e7c0404faa026015a740340c78a41
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-29 22:17:14 +0700

    Update buyprem.py

--- Kymang/modules/buyprem.py
+++ Kymang/modules/buyprem.py
@@ -13,13 +13,18 @@ now = datetime.datetime.now(timezone("Asia/Jakarta"))
 expired = now + relativedelta(days=int(30))
 DATE = expired.strftime("%d-%B-%Y")  
 
+@bot.on_callback_query(filters.regex("month0"))
+async def month0(c: Bot, cb: CallbackQuery):
+    await cb.edit_message_text("Null", reply_markup=SATU_BULAN)
+
 C10 = """
 <b>🛒 Keranjang</b>
-• <b>Order:</b> Force Sub
+• Fsub Premium 1 Bulan
 
 [•] <b>Tanggal Kadaluarsa:</b> {}
 
-<b>Total Harga: Rp.40.000</b>
+• <b>Harga: Rp.40.000</b>
+• <b>Total Harga: Rp.40.000</b>
 """
 
 SATU_BULAN = InlineKeyboardMarkup(
@@ -38,16 +43,37 @@ SATU_BULAN = InlineKeyboardMarkup(
     ]
 )
 
-C15 = """
-<b>🛒 Keranjang</b>
-• <b>Order:</b> Force Sub
-
-[•] <b>Tanggal Kadaluarsa:</b> {}
-
-<b>Total Harga: Rp.40.000</b>
-"""
-
 @bot.on_callback_query(filters.regex("month1"))
 async def month1(c: Bot, cb: CallbackQuery):
     await cb.edit_message_text(C10.format(DATE), reply_markup=SATU_BULAN)
+    
+C15 = """
+<b>🛒 Keranjang</b>
+• Fsub Premium 2 Bulan
 
+[•] <b>Tanggal Kadaluarsa:</b> {}
+
+• <b>Harga: Rp.80.000</b>
+• <b>Diskon: -Rp.5.000</b>
+• <b>Total Harga: Rp.75.000</b>
+"""
+
+DUA_BULAN = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("📅Bulan", callback_data="JK10"),
+        ],
+        [
+            InlineKeyboardButton("-1 Bulan", callback_data="month0"),
+            InlineKeyboardButton("+1 Bulan", callback_data="month2"),
+        ],
+        [
+            InlineKeyboardButton("❌ Batalkan", callback_data="mulai"),
+            InlineKeyboardButton("✅ Lanjutkan", callback_data="mulai"),
+        ],
+    ]
+)
+
+@bot.on_callback_query(filters.regex("month2"))
+async def month2(c: Bot, cb: CallbackQuery):
+    await cb.edit_message_text(C15.format(DATE), reply_markup=SATU_BULAN)
