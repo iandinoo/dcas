32be8317a3fc18386a444e1f0f6d6c5dbb9d2dcd
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-29 22:43:43 +0700

    Update config.py

--- Kymang/config.py
+++ Kymang/config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "3f15b21827506cb63890f756743be15f")
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://fsubprem:itsiannnn@fsubprem.lgqre.mongodb.net/?retryWrites=true&w=majority&appName=fsubprem")
 ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1846991467").split())]
 MEMBER = [int(x) for x in (os.environ.get("MEMBER", "800000").split())]
-LOG_GRP = int(os.environ.get("LOG_GRP", "-1002350694478"))
+LOG_GRP = int(os.environ.get("LOG_GRP", "-1002435919081"))
 BOT_ID = int(os.environ.get("BOT_ID", "7368858350"))
 
 KITA = [int(x) for x in (os.environ.get("KITA", "1846991467").split())]
