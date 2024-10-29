db00aac847934603caaa90dd8e984a338c360586
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-29 22:01:48 +0700

    Update buyprem.py

--- Kymang/modules/buyprem.py
+++ Kymang/modules/buyprem.py
@@ -7,6 +7,15 @@ from datetime import datetime, timedelta
 from pyrogram import Client, filters
 from pyrogram.types import *
 
+C10 = """
+<b>🛒 Keranjang</b>
+• <b>Order:</b> Force Sub
+
+[•] <b>Tanggal Kadaluarsa:</b> {}
+
+<b>Total Harga: Rp.40.000</b>
+"""
+
 @bot.on_callback_query(filters.regex("month1"))
 async def month1(c: Bot, cb: CallbackQuery):
     await cb.edit_message_text(C10, reply_markup=KL10)
