a94205c827db57dd148e076c8faf6cf6b1fc18dc
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-29 21:52:24 +0700

    Create buyprem.py

---
+++ Kymang/modules/buyprem.py
@@ -0,0 +1,8 @@
+import asyncio
+import importlib
+import logging
+import os
+from datetime import datetime, timedelta
+from pykeyboard import InlineKeyboard
+from pyrogram import Client, filters
+from pyrogram.types import *
